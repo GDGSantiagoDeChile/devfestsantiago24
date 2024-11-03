bddc05e9c101f52abf3982e3d1a4d5c518a58d38
Armando Picón <picondev@gmail.com>
2024-11-03 18:17:01 -0300

    update references

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { useRouter } from 'next/router';
 
 export default function Document() {
+	const { basePath } = useRouter();
+
 	return (
 		<Html lang='es'>
-			<Head />
+			<Head>
+				{/* Ruta del favicon con basePath */}
+				<link rel="icon" href={`${basePath}/favicon.ico`} />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
